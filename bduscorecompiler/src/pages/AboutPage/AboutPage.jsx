d277835cd5525328a52a397bdc8a123ad0182ea3
Ezhilsivaraj <112194071+ezhil-003@users.noreply.github.com>
2024-01-15 18:43:43 +0530

    Add files via upload

--- bduscorecompiler/src/pages/AboutPage/AboutPage.jsx
+++ bduscorecompiler/src/pages/AboutPage/AboutPage.jsx
@@ -1,1 +1,37 @@
+import React from 'react'
+import TopBar from '../../components/TopBar/TopBar.jsx';
+import { Link,NavLink } from "react-router-dom";
 
+const AboutPage = () => {
+  return (
+    <>
+      <TopBar />
+      <h1>This is About Page</h1>
+      <nav>
+        <ul>
+        <li>
+            <NavLink to="/">Student Login</NavLink>
+            </li>
+            <li>
+          <li>
+          <NavLink to="/Faculty Login">Faculty Login</NavLink>
+            </li>
+            <li>
+            <NavLink to="/Admin Login">Admin Login</NavLink>
+            </li>
+            <li>
+            <NavLink to="/Support">Support</NavLink>
+            </li>
+            <li>
+            <NavLink to="/Help">Help</NavLink>
+            </li>
+            
+              <a></a>
+            </li>
+          </ul>
+        </nav>
+    </>
+  )
+}
+
+export default AboutPage;
